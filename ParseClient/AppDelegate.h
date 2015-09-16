aef8a77bf73ae61b4d2947e4035c92db1edee3e3
Devin Bhushan <devinb@yahoo-inc.com>
2015-09-16 12:18:13 -0700

    Initial commit

---
+++ ParseClient/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ParseClient
+//
+//  Created by Devin Bhushan on 9/16/15.
+//  Copyright © 2015 Yahoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ParseClient/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ParseClient
+//
+//  Created by Devin Bhushan on 9/16/15.
+//  Copyright © 2015 Yahoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
